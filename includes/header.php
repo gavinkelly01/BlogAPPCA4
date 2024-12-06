215009d278f5e1f5ec9e464d4191787c4c8cc964
Gavin Kelly <124623076+gavinkelly01@users.noreply.github.com>
2024-12-06 01:38:05 +0000

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,11 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+include_once('functions.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +14,15 @@
     <title>Blog</title>
 </head>
 <body>
-    <header>
-        <nav>
-            <a href="index.php">Home</a>
-            <?php if (isLoggedIn()): ?>
-                <a href="logout.php">Logout</a>
-            <?php else: ?>
-                <a href="login.php">Login</a> | <a href="register.php">Register</a>
-            <?php endif; ?>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <a href="index.php">Home</a>
+        <?php if (isLoggedIn()): ?>
+            <a href="logout.php">Logout</a>
+        <?php else: ?>
+            <a href="login.php">Login</a> | <a href="register.php">Register</a>
+        <?php endif; ?>
+    </nav>
+</header>
+</body>
+</html>
