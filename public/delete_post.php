36a6426ff21f960bb660ca5628ad75ba67f0c3aa
Gavin Kelly <124623076+gavinkelly01@users.noreply.github.com>
2024-12-06 01:36:44 +0000

    Update delete_post.php

--- public/delete_post.php
+++ public/delete_post.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include '../includes/db.php';
-
+include '../includes/functions.php';
 if (!isset($_SESSION['user_id']) || !isset($_GET['id'])) {
     header("Location: login.php");
     exit;
