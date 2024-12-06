e6e946565462210651254755d852a93f28e6a145
Gavin Kelly <124623076+gavinkelly01@users.noreply.github.com>
2024-12-06 01:25:26 +0000

    Update register.php

--- public/register.php
+++ public/register.php
@@ -1,24 +1,23 @@
 <?php
 session_start();
 include '../includes/db.php';
-include '../includes/functions.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     $db = getDB();
-    $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
-    $stmt->execute([':username' => $username, ':password' => $password]);
+    $db->prepare("INSERT INTO users (username, password) VALUES ('$username', '$password')")->execute();
 
     header("Location: login.php");
+    exit;
 }
 ?>
 
 <form method="POST">
     <label>Username:</label><br>
-    <input type="text" name="username" ><br>
+    <input type="text" name="username"><br>
     <label>Password:</label><br>
-    <input type="password" name="password" ><br>
+    <input type="password" name="password"><br>
     <button type="submit">Register</button>
 </form>
