99f92f915abb11dd24364f05cec7980fb8c89964
Gavin Kelly <124623076+gavinkelly01@users.noreply.github.com>
2024-12-06 01:37:00 +0000

    Update edit_post.php

--- public/edit_post.php
+++ public/edit_post.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include '../includes/db.php';
-
+include '../includes/functions.php';
 if (!isset($_SESSION['user_id']) || !isset($_GET['id'])) {
     header("Location: login.php");
     exit;
@@ -36,4 +36,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <textarea name="content" required><?php echo $post['content']; ?></textarea><br>
     <button type="submit">Update Post</button>
 </form>
+
+
 
